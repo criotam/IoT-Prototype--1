8fb0a3b6aabdabe49ab28fd19c7fdc60964c372c
ARGHA SEN <arghasen10@gmail.com>
2018-05-24 11:15:13 +0530

    Add files via upload
    Arduino code for Loadcell calibration and measurement

---
+++ loadcell calibration/loadcell_check/loadcell_check.ino/loadcell_check.ino.ino
@@ -0,0 +1,29 @@
+
+
+#include "HX711.h"
+
+#define DOUT  3
+#define CLK  2
+
+HX711 scale(DOUT, CLK);
+
+float calibration_factor = -3150;
+void setup() {
+  Serial.begin(9600);
+
+  scale.set_scale();
+  scale.tare();  //Reset the scale to 0
+
+  
+}
+
+void loop() {
+
+  scale.set_scale(calibration_factor);
+
+  Serial.print("Reading: ");
+  Serial.print(scale.get_units()*0.453592, 1);
+  Serial.print(" kgs");
+  Serial.println();
+
+}
