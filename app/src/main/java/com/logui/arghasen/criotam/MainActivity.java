ef1958964bdc2605f596236bc9605d064467fc8e
arghasen10 <arghasen10@gmail.com>
2018-06-05 21:07:42 +0530

    android application welcome page

---
+++ app/src/main/java/com/logui/arghasen/criotam/MainActivity.java
@@ -0,0 +1,41 @@
+package com.logui.arghasen.criotam;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private Button login;
+    private Button signup;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.welcome_page);
+        login = (Button) findViewById(R.id.btnLogin);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openlogin();
+            }
+        });
+        signup = (Button)findViewById(R.id.btnSignup);
+        signup.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                opensignup();
+            }
+        });
+    }
+    public void openlogin(){
+        Intent intent = new Intent(this , login.class);
+        startActivity(intent);
+    }
+    public void opensignup(){
+        Intent intent = new Intent(this,signup.class);
+        startActivity(intent);
+    }
+
+
+}
