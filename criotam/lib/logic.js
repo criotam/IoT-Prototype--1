a80f56543b82a7e23e4c8ad3d06be7ffc7fcb02a
Rahul Jangid <rahultz229@gmail.com>
2018-06-12 12:09:43 +0530

    Changes made in permissions file, issued 3 identities

--- criotam/lib/logic.js
+++ criotam/lib/logic.js
@@ -1,4 +1,27 @@
 /**
- * Create Flight Transaction
+ * Create Scientist Transaction
  * @param {org.criotam.prototype.experiment.Experiment}
  */
+
+/* const BusinessNetworkConnection = require('composer-client').BusinessNetworkConnection;
+
+this.bizNetworkConnection = new BusinessNetworkConnection();
+this.cardName = config.get('cardName');
+this.businessNetworkIdentifier = config.get('businessNetworkIdentifier');
+
+this.bizNetworkConnection.connect(this.cardName)
+.then((result) => {
+  this.businessNetworkDefinition = result;
+});
+
+this.bizNetworkConnection.getAssetRegistry('net.biz.digitalPropertyNetwork.LandTitle')
+.then((result) => {
+    this.titlesRegistry = result;
+});
+
+
+let factory = this.businessNetworkDefinition.getFactory();
+owner = factory.newResource('net.biz.digitalPropertyNetwork', 'Person', 'PID:1234567890');
+owner.firstName = 'Fred';
+owner.lastName = 'Bloggs';
+*/
