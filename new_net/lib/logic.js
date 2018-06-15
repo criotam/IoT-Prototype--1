eea2f8ef964eb4b5d5d888347e2d4153c759cdd6
Rahul Jangid <rahultz229@gmail.com>
2018-06-15 12:18:39 +0530

    updated chaincode

--- new_net/lib/logic.js
+++ new_net/lib/logic.js
@@ -22,37 +22,49 @@
  * @param {org.criotam.prototype.sensor.readSensor} readSensor
  * @transaction
  */
+
+ 
 async function readSensor(tx) {
+    var NS = 'org.criotam.prototype.sensor'
     // Save the old value of the asset.
     const oldValue = tx.sensor.value;
-    console.log(tx.sensor.value);
     // Update the asset with the new value.
     tx.sensor.value = tx.newValue;
 
     // Get the asset registry for the asset.
     const assetRegistry = await getAssetRegistry('org.criotam.prototype.sensor.Sensor');
     // Update the asset in the asset registry.
-    await assetRegistry.update(tx.sensor.value);
+    await assetRegistry.update(tx.sensor);
 
     // Emit an event for the modified asset.
-    let event = getFactory().newEvent('org.criotam.prototype.sensor', 'sensorRead');
+    let event = getFactory().newEvent(NS, 'sensorRead');
     event.sensor = tx.sensor;
     event.oldValue = oldValue;
     event.newValue = tx.newValue;
     emit(event);
 }
 
+/**
+ * readSensor
+ * @param {org.criotam.prototype.sensor.addPlayer} addPlayer
+ * @transaction
+ */
 
-async function addPlayer(tx) {
-    const assetRegistry = await getAssetRegistry('org.criotam.prototype.sensor');
-    let event = getFactory().newEvent('org.criotam.prototype.sensor', 'addedPlayer');
-    event.player = tx.player;
-    event.player_id = tx.player_id;
-    event.player_name = tx.player_name;
-    event.player_age = tx.player_age;
-    event.player_weight = tx.player_weight;
-    event.player_height = tx.player_height;
-    event.plaeyr_sex = tx.player_sex;
+async function addPlayer(playerData) {
+    var NS = 'org.criotam.prototype.sensor'
+    const assetRegistry = await getAssetRegistry('org.criotam.prototype.sensor.addPlayer');
+    var factory = getFactory();
+    var Player = factory.newResources(NS, "Player", playerData.player);
+    Player.player_id = playerData.playerId;
+    Player.player_name = playerData.player_name;
+    Player.player_age = playerData.player_age;
+    Player.player_weight = playerData.player_weight;
+    Player.player_height = playerData.player_height;
+    Player.player_sex = playerData.player_sex;
+
+    await assetRegistry.add(Player);
+
+    let event = factory.newEvent(NS, 'addedPlayer');
+    event.player = playerData.player
     emit(event);
-
 }
